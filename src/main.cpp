11abd6378d78be81aef4306281babc320522b614
Yi-Ping Pan <pan.yiping.fi@gmail.com>
2025-04-07 20:41:04 +0800

    Extract function signature parser into parser.cpp/hpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include <clang-c/Index.h>
-
+#include "parser.h"
 #include <cassert>
+#include <clang-c/Index.h>
 #include <cstddef>
 #include <cstdio>
 #include <iostream>
@@ -13,51 +13,6 @@
 
 constexpr std::size_t BUFFER_SIZE = 512;
 constexpr int EXPECTED_ARG_COUNT = 3;
-
-std::string_view trim(std::string_view sv) {
-  const size_t start = sv.find_first_not_of(" \t\n\r");
-  if (start == std::string_view::npos)
-    return {};
-
-  const size_t end = sv.find_last_not_of(" \t\n\r");
-  return sv.substr(start, end - start + 1);
-}
-
-struct Signature {
-  std::string retType;
-  std::vector<std::string> argType;
-};
-
-bool parseFunctionSignature(std::string_view input, Signature &output) {
-  size_t parenOpen = input.find('(');
-  size_t parenClose = input.find(')', parenOpen);
-
-  if (parenOpen == std::string_view::npos ||
-      parenClose == std::string_view::npos || parenClose <= parenOpen) {
-    std::fprintf(stderr, "Invalid function signature: '%.*s'\n",
-                 (int)input.size(), input.data());
-    return false;
-  }
-
-  output.retType = input.substr(0, parenOpen);
-  std::cout << "retType: " << output.retType << std::endl;
-
-  size_t start = parenOpen + 1;
-  while (start < input.size()) {
-    size_t end = input.find_first_of(",)", start);
-    std::string_view token = input.substr(start, end - start);
-    std::string_view cleaned = trim(token);
-    if (!cleaned.empty())
-      output.argType.push_back(std::string(cleaned));
-
-    if (end == std::string_view::npos)
-      break;
-
-    start = end + 1;
-  }
-
-  return true;
-}
 
 CXChildVisitResult visitor(CXCursor cursor, CXCursor parent,
                            CXClientData client_data) {
@@ -130,10 +85,6 @@ std::vector<std::string> detectSystemIncludePaths() {
     }
   }
 
-  if (pclose(pipe) != 0) { // Check for errors when closing the pipe
-    std::fprintf(stderr,
-                 "Error closing pipe for clang include path detection.\n");
-  }
   pclose(pipe);
   return includePaths;
 }
